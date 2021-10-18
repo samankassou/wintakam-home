74ada8774130a86a076c32d96c202a9d5a5d46c4
samankassou <samankassoufoulla@gmail.com>
2021-10-18 21:33:53 +0100

    add the mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 const toggler = document.getElementById("toggler")
+const menuMobile = document.getElementById("menu-mobile")
 
 toggler.addEventListener("click", ()=>{
     toggler.classList.toggle("active")
+    menuMobile.classList.toggle("active")
 })
